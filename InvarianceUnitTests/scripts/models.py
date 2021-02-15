72578ed9d5fb93b13234bf4933cad1408aa3b2a5
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-02-15 14:42:23 -0500

    add rex, and also add ib versions of erm, irm, & andmask

--- InvarianceUnitTests/scripts/models.py
+++ InvarianceUnitTests/scripts/models.py
@@ -72,6 +72,132 @@ class ERM(Model):
         return self.network(x)
 
 
+class IB_ERM(Model):
+    def __init__(self, in_features, out_features, task, hparams="default"):
+        self.HPARAMS = {}
+        self.HPARAMS["lr"] = (1e-3, 10**random.uniform(-4, -2))
+        self.HPARAMS['wd'] = (0., 10**random.uniform(-6, -2))
+
+        super().__init__(in_features, out_features, task, hparams)
+
+        self.optimizer = torch.optim.Adam(
+            self.network.parameters(),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams["wd"])
+
+    def fit(self, envs, num_iterations, callback=False):
+        x = torch.cat([xe for xe, ye in envs["train"]["envs"]])
+        y = torch.cat([ye for xe, ye in envs["train"]["envs"]])
+
+        for epoch in range(num_iterations):
+            self.optimizer.zero_grad()
+            logits = self.network(x)
+            loss = self.loss(logits, y) + .9 * logits.var(0).mean()
+            loss.backward()
+            self.optimizer.step()
+
+            if callback:
+                # compute errors
+                utils.compute_errors(self, envs)
+
+    def predict(self, x):
+        return self.network(x)
+
+class REx(Model):
+    """
+    Abstract class for REx
+    """
+
+    def __init__(
+            self, in_features, out_features, task, hparams="default", version=1):
+        self.HPARAMS = {}
+        self.HPARAMS["lr"] = (1e-3, 10**random.uniform(-4, -2))
+        self.HPARAMS['wd'] = (0., 10**random.uniform(-6, -2))
+        self.HPARAMS['irm_lambda'] = (0.9, 1 - 10**random.uniform(-3, -.3))
+
+        super().__init__(in_features, out_features, task, hparams)
+        self.version = version
+
+        self.network = self.IRMLayer(self.network)
+        self.net_parameters, self.net_dummies = self.find_parameters(
+            self.network)
+
+        self.optimizer = torch.optim.Adam(
+            self.net_parameters,
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams["wd"])
+
+    def find_parameters(self, network):
+        """
+        Alternative to network.parameters() to separate real parameters
+        from dummmies.
+        """
+        parameters = []
+        dummies = []
+
+        for name, param in network.named_parameters():
+            if "dummy" in name:
+                dummies.append(param)
+            else:
+                parameters.append(param)
+        return parameters, dummies
+
+    class IRMLayer(torch.nn.Module):
+        """
+        Add a "multiply by one and sum zero" dummy operation to
+        any layer. Then you can take gradients with respect these
+        dummies. Often applied to Linear and Conv2d layers.
+        """
+
+        def __init__(self, layer):
+            super().__init__()
+            self.layer = layer
+            self.dummy_mul = torch.nn.Parameter(torch.Tensor([1.0]))
+            self.dummy_sum = torch.nn.Parameter(torch.Tensor([0.0]))
+
+        def forward(self, x):
+            return self.layer(x) * self.dummy_mul + self.dummy_sum
+
+    def fit(self, envs, num_iterations, callback=False):
+        for epoch in range(num_iterations):
+            losses_env = []
+            gradients_env = []
+            for x, y in envs["train"]["envs"]:
+                losses_env.append(self.loss(self.network(x), y))
+                gradients_env.append(grad(
+                    losses_env[-1], self.net_dummies, create_graph=True))
+
+            # Average loss across envs
+            losses_avg = sum(losses_env) / len(losses_env)
+            penalty = torch.stack([lo for lo in losses_env]).var()
+            """
+            gradients_avg = grad(
+                losses_avg, self.net_dummies, create_graph=True)
+
+            penalty = 0
+            for gradients_this_env in gradients_env:
+                for g_env, g_avg in zip(gradients_this_env, gradients_avg):
+                    if self.version == 1:
+                        penalty += g_env.pow(2).sum()
+                    else:
+                        raise NotImplementedError
+            #"""
+
+            obj = (1 - self.hparams["irm_lambda"]) * losses_avg
+            obj += self.hparams["irm_lambda"] * penalty
+
+            self.optimizer.zero_grad()
+            obj.backward()
+            self.optimizer.step()
+
+            if callback:
+                # compute errors
+                utils.compute_errors(self, envs)
+
+    def predict(self, x):
+        return self.network(x)
+
+
 class IRM(Model):
     """
     Abstract class for IRM
@@ -174,6 +300,108 @@ class IRMv1(IRM):
         super().__init__(in_features, out_features, task, hparams, version=1)
 
 
+class IB_IRM(Model):
+    """
+    Abstract class for IRM
+    """
+
+    def __init__(
+            self, in_features, out_features, task, hparams="default", version=1):
+        self.HPARAMS = {}
+        self.HPARAMS["lr"] = (1e-3, 10**random.uniform(-4, -2))
+        self.HPARAMS['wd'] = (0., 10**random.uniform(-6, -2))
+        self.HPARAMS['irm_lambda'] = (0.9, 1 - 10**random.uniform(-3, -.3))
+
+        super().__init__(in_features, out_features, task, hparams)
+        self.version = version
+
+        self.network = self.IRMLayer(self.network)
+        self.net_parameters, self.net_dummies = self.find_parameters(
+            self.network)
+
+        self.optimizer = torch.optim.Adam(
+            self.net_parameters,
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams["wd"])
+
+    def find_parameters(self, network):
+        """
+        Alternative to network.parameters() to separate real parameters
+        from dummmies.
+        """
+        parameters = []
+        dummies = []
+
+        for name, param in network.named_parameters():
+            if "dummy" in name:
+                dummies.append(param)
+            else:
+                parameters.append(param)
+        return parameters, dummies
+
+    class IRMLayer(torch.nn.Module):
+        """
+        Add a "multiply by one and sum zero" dummy operation to
+        any layer. Then you can take gradients with respect these
+        dummies. Often applied to Linear and Conv2d layers.
+        """
+
+        def __init__(self, layer):
+            super().__init__()
+            self.layer = layer
+            self.dummy_mul = torch.nn.Parameter(torch.Tensor([1.0]))
+            self.dummy_sum = torch.nn.Parameter(torch.Tensor([0.0]))
+
+        def forward(self, x):
+            return self.layer(x) * self.dummy_mul + self.dummy_sum
+
+    def fit(self, envs, num_iterations, callback=False):
+        for epoch in range(num_iterations):
+            losses_env = []
+            gradients_env = []
+            logits_env = []
+            for x, y in envs["train"]["envs"]:
+                logits = self.network(x)
+                logits_env.append(logits)
+                losses_env.append(self.loss(self.network(x), y))
+                gradients_env.append(grad(
+                    losses_env[-1], self.net_dummies, create_graph=True))
+
+            # penalty per env
+            logit_penalty = torch.stack(logits_env).var(1).mean()
+
+            # single
+            #logit_penalty = torch.cat(logits_env).var(0).mean()
+
+            # Average loss across envs
+            losses_avg = sum(losses_env) / len(losses_env)
+            gradients_avg = grad(
+                losses_avg, self.net_dummies, create_graph=True)
+
+            penalty = 0
+            for gradients_this_env in gradients_env:
+                for g_env, g_avg in zip(gradients_this_env, gradients_avg):
+                    if self.version == 1:
+                        penalty += g_env.pow(2).sum()
+                    else:
+                        raise NotImplementedError
+
+            obj = (1 - self.hparams["irm_lambda"]) * losses_avg
+            obj += self.hparams["irm_lambda"] * penalty
+
+            obj += self.hparams["irm_lambda"] * logit_penalty
+
+            self.optimizer.zero_grad()
+            obj.backward()
+            self.optimizer.step()
+
+            if callback:
+                # compute errors
+                utils.compute_errors(self, envs)
+
+    def predict(self, x):
+        return self.network(x)
+
 class AndMask(Model):
     """
     AndMask: Masks the grqdients features for which 
@@ -192,6 +420,73 @@ class AndMask(Model):
         for epoch in range(num_iterations):
             losses = [self.loss(self.network(x), y)
                       for x, y in envs["train"]["envs"]]
+            self.mask_step(
+                losses, list(self.parameters()),
+                tau=self.hparams["tau"],
+                wd=self.hparams["wd"],
+                lr=self.hparams["lr"]
+            )
+
+            if callback:
+                # compute errors
+                utils.compute_errors(self, envs)
+
+    def predict(self, x):
+        return self.network(x)
+
+    def mask_step(self, losses, parameters, tau=0.9, wd=0.1, lr=1e-3):
+        with torch.no_grad():
+            gradients = []
+            for loss in losses:
+                gradients.append(list(torch.autograd.grad(loss, parameters)))
+                gradients[-1][0] = gradients[-1][0] / gradients[-1][0].norm()
+
+            for ge_all, parameter in zip(zip(*gradients), parameters):
+                # environment-wise gradients (num_environments x num_parameters)
+                ge_cat = torch.cat(ge_all)
+
+                # treat scalar parameters also as matrices
+                if ge_cat.dim() == 1:
+                    ge_cat = ge_cat.view(len(losses), -1)
+
+                # creates a mask with zeros on weak features
+                mask = (torch.abs(torch.sign(ge_cat).sum(0))
+                        > len(losses) * tau).int()
+
+                # mean gradient (1 x num_parameters)
+                g_mean = ge_cat.mean(0, keepdim=True)
+
+                # apply the mask
+                g_masked = mask * g_mean
+
+                # update
+                parameter.data = parameter.data - lr * g_masked \
+                    - lr * wd * parameter.data
+
+
+class IB_AndMask(Model):
+    """
+    AndMask: Masks the grqdients features for which 
+    the gradients signs across envs disagree more than 'tau'
+    From https://arxiv.org/abs/2009.00329
+    """
+
+    def __init__(self, in_features, out_features, task, hparams="default"):
+        self.HPARAMS = {}
+        self.HPARAMS["lr"] = (1e-3, 10**random.uniform(-4, 0))
+        self.HPARAMS['wd'] = (0., 10**random.uniform(-5, 0))
+        self.HPARAMS["tau"] = (0.9, random.uniform(0.8, 1))
+        super().__init__(in_features, out_features, task, hparams)
+
+    def fit(self, envs, num_iterations, callback=False):
+        for epoch in range(num_iterations):
+            logits = []
+            losses = []
+            for x, y in envs["train"]["envs"]:
+                logit = self.network(x)
+                #logits.append(logit)
+                loss = self.loss(logit, y) + .9 * logit.var(0).mean()
+                losses.append(loss)
             self.mask_step(
                 losses, list(self.parameters()),
                 tau=self.hparams["tau"],
@@ -286,8 +581,12 @@ class IGA(Model):
 
 MODELS = {
     "ERM": ERM,
+    "REx": REx,
+    "IB_ERM": IB_ERM,
     "IRMv1": IRMv1,
+    "IB_IRM": IB_IRM,
     "ANDMask": AndMask,
+    "IB_ANDMask": IB_AndMask,
     "IGA": IGA,
     "Oracle": ERM
 }
