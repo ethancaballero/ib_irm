66faecf88e4560bd2124bd0f0ecb82454f8ea1c5
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-02-23 13:48:33 -0500

    user

--- InvarianceUnitTests/job2.sh
+++ InvarianceUnitTests/job2.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1                     # Ask for 1 GPU
 #SBATCH --mem=32G                        # Ask for 32 GB of RAM
 #SBATCH --time=3:00:00                   # The job will run for 3 hours
-#SBATCH -o /scratch/<user>/slurm-%j.out  # Write the log in $SCRATCH
+#SBATCH -o /scratch/ethancab/slurm-%j.out  # Write the log in $SCRATCH
 
 cd $SLURM_TMPDIR
 
