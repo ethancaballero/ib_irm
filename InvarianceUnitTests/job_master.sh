4b50486a0afb2bffa011851c86e5e624565a073a
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-02-25 05:22:29 -0500

    finally?

--- InvarianceUnitTests/job_master.sh
+++ InvarianceUnitTests/job_master.sh
@@ -8,8 +8,8 @@ done
 
 #for i in `seq 0 1 19`; do
 for i in `seq 0 1 2`; do
-    #sbatch job_node.sh --m_start $i --m_end $(( $i + 1 ))
-    sbatch job_node.sh $i $(( $i + 1 ))
+    sbatch job_node.sh --m_start $i --m_end $(( $i + 1 ))
+    #sbatch job_node.sh $i $(( $i + 1 ))
     #echo $i + 1
     #echo $(( $i + 2 ))
 done
--- InvarianceUnitTests/job_node.sh
+++ InvarianceUnitTests/job_node.sh
@@ -10,6 +10,88 @@
 
 # max cpu is 40 and max mem is 752G
 
+argparse(){
+    argparser=$(mktemp 2>/dev/null || mktemp -t argparser)
+    cat > "$argparser" <<EOF
+from __future__ import print_function
+import sys
+import argparse
+import os
+class MyArgumentParser(argparse.ArgumentParser):
+    def print_help(self, file=None):
+        """Print help and exit with error"""
+        super(MyArgumentParser, self).print_help(file=file)
+        sys.exit(1)
+parser = MyArgumentParser(prog=os.path.basename("$0"),
+            description="""$ARGPARSE_DESCRIPTION""")
+EOF
+
+    # stdin to this function should contain the parser definition
+    cat >> "$argparser"
+
+    cat >> "$argparser" <<EOF
+args = parser.parse_args()
+for arg in [a for a in dir(args) if not a.startswith('_')]:
+    key = arg.upper()
+    value = getattr(args, arg, None)
+    if isinstance(value, bool) or value is None:
+        print('{0}="{1}";'.format(key, 'yes' if value else ''))
+    elif isinstance(value, list):
+        print('{0}=({1});'.format(key, ' '.join('"{0}"'.format(s) for s in value)))
+    else:
+        print('{0}="{1}";'.format(key, value))
+EOF
+
+    # Define variables corresponding to the options if the args can be
+    # parsed without errors; otherwise, print the text of the error
+    # message.
+    if python "$argparser" "$@" &> /dev/null; then
+        eval $(python "$argparser" "$@")
+        retval=0
+    else
+        python "$argparser" "$@"
+        retval=1
+    fi
+
+    rm "$argparser"
+    return $retval
+}
+
+# print a script template when this script is executed
+if [[ $0 == *argparse.bash ]]; then
+    cat <<FOO
+#!/usr/bin/env bash
+source \$(dirname \$0)/argparse.bash || exit 1
+argparse "\$@" <<EOF || exit 1
+parser.add_argument('infile')
+parser.add_argument('-o', '--outfile')
+EOF
+echo "INFILE: \${INFILE}"
+echo "OUTFILE: \${OUTFILE}"
+FOO
+fi
+
+argparse "$@" <<EOF || exit 1
+parser.add_argument('--m_start', default=0, type=int,
+                    help='')
+parser.add_argument('--m_end', default=20, type=int,
+                    help='')
+
+parser.add_argument('--d_start', default=0, type=int,
+                    help='')
+parser.add_argument('--d_end', default=50, type=int,
+                    help='')
+EOF
+
+#echo the answer: $M_START
+#echo the answer: $M_END
+
+module load python/3.8
+cd /home/ethancab
+source invariance_env/bin/activate
+cd /home/ethancab/research/invariance_unit_test/ib_irm/InvarianceUnitTests
 
 #python scripts/sweep.py --skip_confirmation True --models ERM --datasets Example2 --num_samples 2 --m_start $M_START --m_end $M_END --d_start 0 --d_end 50
-python scripts/sweep.py --skip_confirmation True --models ERM --datasets Example2 --num_samples 2 --m_start $1 --m_end $2 --d_start 0 --d_end 1
+python scripts/sweep.py --skip_confirmation True --models ERM --datasets Example2 --num_samples 2 --m_start $M_START --m_end $M_END --d_start 0 --d_end 1
+
+cp -R /home/ethancab/research/invariance_unit_test/ib_irm/InvarianceUnitTests/results $SCRATCH
