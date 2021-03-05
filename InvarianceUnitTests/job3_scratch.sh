ee77adba5881f48746260569d5c5ee3b7160e020
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-03-05 00:08:28 -0500

    debug

---
+++ InvarianceUnitTests/job3_scratch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=rrg-bengioy-ad         # Yoshua pays for your job
+#SBATCH --cpus-per-task=6                # Ask for 6 CPUs
+#SBATCH --gres=gpu:1                     # Ask for 1 GPU
+#SBATCH --mem=32G                        # Ask for 32 GB of RAM
+#SBATCH --time=3:00:00                   # The job will run for 3 hours
+#SBATCH -o /scratch/ethancab/slurm-%j.out  # Write the log in $SCRATCH
+
+#cd /home/ethancab/research/_idk
+
+#/scratch/ethancab
+
+echo "some text" >> /scratch/ethancab/myfile.txt
+
+# 5. Copy whatever you want to save on $SCRATCH
+#cp /home/ethancab/research/_idk/myfile.txt $SCRATCH
