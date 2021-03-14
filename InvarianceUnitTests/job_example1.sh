ce33bad9d64166fffbfa6bf4211d8fc0e4508542
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-03-14 16:05:23 -0400

    job_example

--- InvarianceUnitTests/job_example1.sh
+++ InvarianceUnitTests/job_example1.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=40                # Ask for 40 CPUs
 #SBATCH --gres=gpu:0                     # Ask for 0 GPU
 #SBATCH --mem=752G                        # Ask for 752 GB of RAM
-#SBATCH --time=0:30:00                   # The job will run for 3 hours
+#SBATCH --time=28:00:00                   # The job will run for 3 hours
 #SBATCH -o /scratch/ethancab/slurm-%j.out  # Write the log in $SCRATCH
 
 # 1. Create your environement locally
@@ -11,4 +11,4 @@ module load python/3.8
 cd /home/ethancab
 source invariance_env/bin/activate
 cd /home/ethancab/research/invariance_unit_test/ib_irm/InvarianceUnitTests
-python scripts/sweep_outer.py --skip_confirmation True --models IB_IRM IB_ERM --datasets Example1 --scratch_dir "/scratch/ethancab"
+python scripts/sweep_outer.py --skip_confirmation True --models IB_IRM IB_ERM --datasets Example1 --scratch_dir "/scratch/ethancab/res_full/example1"
