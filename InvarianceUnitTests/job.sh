e85d7c95ce0629c006664a5f337388dbf069c6ff
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-04-20 07:23:47 -0400

    add ib_bool as flag for job.sh

--- InvarianceUnitTests/job.sh
+++ InvarianceUnitTests/job.sh
@@ -5,7 +5,8 @@
 #SBATCH --mem=752G                        # Ask for 752 GB of RAM
 #SBATCH --time=75:00:00                   # The job will run for 3 hours
 
-new_hparam_interval="True"
+new_hparam_interval="True" # this just determines whether irm_loss is normalized by number of envs
+ib_bool="False" # whether or not ib_on is used as a hparam
 mod_folder_name=""
 d_start="0"
 d_end="20"
@@ -24,6 +25,10 @@ do
 case $i in
     -nhi=*|--new_hparam_interval=*)
     new_hparam_interval="${i#*=}"
+    shift # past argument=value
+    ;;
+    -ibb=*|--ib_bool=*)
+    ib_bool="${i#*=}"
     shift # past argument=value
     ;;
     -ne=*|--n_envs=*)
@@ -69,6 +74,7 @@ scratch_dir="/scratch/ethancab/res_full_${n_envs}${mod_folder_name}/${datasets}/
 #scratch_dir="/Users/ethancaballero/zzz/debug/res_full_${n_envs}env/${datasets}/${models}"
 
 echo "$new_hparam_interval"
+echo "$ib_bool"
 echo "$n_envs"
 echo "$models"
 echo "$datasets"
@@ -84,5 +90,5 @@ module load python/3.8
 cd /home/ethancab
 source invariance_env/bin/activate
 cd /home/ethancab/research/invariance_unit_test/ib_irm/InvarianceUnitTests
-python scripts/sweep_outer.py --skip_confirmation True --new_hparam_interval $new_hparam_interval --n_envs $n_envs --models $models --datasets $datasets --d_start $d_start --d_end $d_end --m_start $m_start --m_end $m_end --scratch_dir $scratch_dir
+python scripts/sweep_outer.py --skip_confirmation True --new_hparam_interval $new_hparam_interval --ib_bool $ib_bool --n_envs $n_envs --models $models --datasets $datasets --d_start $d_start --d_end $d_end --m_start $m_start --m_end $m_end --scratch_dir $scratch_dir
 #python scripts/sweep_outer.py --skip_confirmation True --new_hparam_interval $new_hparam_interval --n_envs $n_envs --models $models --datasets $datasets --scratch_dir $scratch_dir --num_samples 2 --m_start 0 --m_end 1 --d_start 0 --d_end 1
