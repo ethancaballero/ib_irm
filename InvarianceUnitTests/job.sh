269809db4ff2d071a28363b8fe7e0264a1a9fa85
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-04-20 12:33:47 -0400

    intervals to job.sh

--- InvarianceUnitTests/job.sh
+++ InvarianceUnitTests/job.sh
@@ -13,6 +13,11 @@ d_end="20"
 m_start="0"
 m_end="50"
 
+ib_lambda_l="-.05"
+ib_lambda_r="0.0"
+irm_lambda_l="-3.0"
+irm_lambda_r="-.3"
+
 user=$USER
 echo "$user"
 
@@ -63,6 +68,22 @@ case $i in
     mod_folder_name="${i#*=}"
     shift # past argument=value
     ;;
+    -ib_l=*|--ib_lambda_l=*)
+    ib_lambda_l="${i#*=}"
+    shift # past argument=value
+    ;;
+    -ib_r=*|--ib_lambda_r=*)
+    ib_lambda_r="${i#*=}"
+    shift # past argument=value
+    ;;
+    -irm_l=*|--irm_lambda_l=*)
+    irm_lambda_l="${i#*=}"
+    shift # past argument=value
+    ;;
+    -irm_r=*|--irm_lambda_r=*)
+    irm_lambda_r="${i#*=}"
+    shift # past argument=value
+    ;;
     *)
           # unknown option
     ;;
@@ -83,6 +104,11 @@ echo "$models"
 echo "$datasets"
 echo "$scratch_dir"
 
+echo "$ib_lambda_l"
+echo "$ib_lambda_r"
+echo "$irm_lambda_l"
+echo "$irm_lambda_r"
+
 if [[ -n $1 ]]; then
     echo "Argument not recognised"
     exit
@@ -93,5 +119,5 @@ module load python/3.8
 cd $cd1
 source invariance_env/bin/activate
 cd $cd2
-python scripts/sweep_outer.py --skip_confirmation True --new_hparam_interval $new_hparam_interval --ib_bool $ib_bool --n_envs $n_envs --models $models --datasets $datasets --d_start $d_start --d_end $d_end --m_start $m_start --m_end $m_end --scratch_dir $scratch_dir
+python scripts/sweep_outer.py --skip_confirmation True --new_hparam_interval $new_hparam_interval --ib_bool $ib_bool --n_envs $n_envs --models $models --datasets $datasets --d_start $d_start --d_end $d_end --m_start $m_start --m_end $m_end --ib_lambda_l $ib_lambda_l --ib_lambda_r $ib_lambda_r --irm_lambda_l $irm_lambda_l --irm_lambda_r $irm_lambda_r --scratch_dir $scratch_dir
 #python scripts/sweep_outer.py --skip_confirmation True --new_hparam_interval $new_hparam_interval --n_envs $n_envs --models $models --datasets $datasets --scratch_dir $scratch_dir --num_samples 2 --m_start 0 --m_end 1 --d_start 0 --d_end 1
